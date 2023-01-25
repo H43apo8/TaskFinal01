1de0ec18b5fcdbb62f70bc6afcff469b070a5ffe
H43apo8 <H43apo8@yandex.ru>
2023-01-25 14:53:44 +0300

    added the program text to README.md

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 Пример: 
 ["hello", "2", "world", ":-)"]  ->  ["2", ":-)"]*/
 Console.Clear();
-string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
+string[] array1 = new string[5] { "hello", "2", "hello", "world", ":-)" };
 string[] array2 = new string[array1.Length];
 int count = 0;
 
